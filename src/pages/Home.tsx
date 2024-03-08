2b6fd536bb1cc87f522508df80090b88cce2079e
Danish-khan93 <danishshoukat1994@gmail.com>
2024-03-08 22:49:04 +0500

    completed the curd  app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../feature/store";
-import { PAGINATIONTYPE ,POSTTYPE } from "../types/postType";
+import { PAGINATIONTYPE, POSTTYPE } from "../types/postType";
 import { postsFetch } from "../feature/slice/postSllice";
-import { Box, Pagination,Typography } from "@mui/material";
+import { Box, Pagination } from "@mui/material";
 import { Card } from "../component";
 
 const Home = () => {
@@ -16,6 +16,7 @@ const Home = () => {
   });
 
   const postData = useSelector((state: RootState) => state?.post?.post);
+
   useEffect(() => {
     if (!bool && postData.length === 0) {
       dispatch(postsFetch(paginationRef?.current));
@@ -28,7 +29,7 @@ const Home = () => {
   // on page change handle the skip and limit by useRef hook to handle previous and next page
 
   const pageChange = (e: React.ChangeEvent<unknown>, p: number) => {
-    setPage(p)
+    setPage(p);
     console.log(p, e);
     if (p * paginationRef.current.limit >= postData.length) {
       paginationRef.current.skip =
@@ -44,13 +45,16 @@ const Home = () => {
 
   return (
     <Box className="flex flex-col items-center">
-        <Typography className="text-[20px] font-semibold">CURD APP </Typography>
-        <Box className="flex justify-evenly flex-wrap gap-10 mx-[10px] my-[20px]">
-      {postData.slice(  page * paginationRef.current.limit - 10,
-        page * paginationRef.current.limit).map((value: POSTTYPE) => {
-        return <Card key={value?.id} value={value} />;
-      })}
-    </Box>
+      <Box className="flex justify-evenly flex-wrap gap-10 mx-[10px] my-[30px]">
+        {postData
+          .slice(
+            page * paginationRef.current.limit - 10,
+            page * paginationRef.current.limit
+          )
+          .map((value: POSTTYPE) => {
+            return <Card key={value?.id} value={value} />;
+          })}
+      </Box>
       <Box className="my-5">
         <Pagination count={10} onChange={pageChange} />
       </Box>
